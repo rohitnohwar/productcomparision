bd31cbf064ffc9ea47227e3d2673ffd50cda2ae2
rohit <rohit.nohwar@gmail.com>
2022-04-24 03:42:28 +0530

    tredence

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Main from './Components/Main/Main.js';
-import Header from './Components/Header/Header.js';
-import Sidebar from './Components/Sidebar/Sidebar';
-import AddUser from './Components/AddUser/AddUser';
-import AddInvoice from './Components/AddInvoice/AddInvoice.js';
-import AddIssue from './Components/AddIssue/AddIssue.js';
-import AddProject from './Components/AddProject/AddProject.js';
-import AddProduct from './Components/AddProduct/AddProduct.js';
+// import Main from './Components/Main/Main.js';
+// import Header from './Components/Header/Header.js';
+// import Sidebar from './Components/Sidebar/Sidebar';
+// import AddUser from './Components/AddUser/AddUser';
+// import AddInvoice from './Components/AddInvoice/AddInvoice.js';
+// import AddIssue from './Components/AddIssue/AddIssue.js';
+// import AddProject from './Components/AddProject/AddProject.js';
+// import AddProduct from './Components/AddProduct/AddProduct.js';
+
+const Main = React.lazy(() => import('./Components/Main/Main.js'));
+const Header = React.lazy(() => import('./Components/Header/Header.js'));
+const Sidebar = React.lazy(() => import('./Components/Sidebar/Sidebar'));
+const AddUser = React.lazy(() => import('./Components/AddUser/AddUser'));
+const AddInvoice = React.lazy(() => import('./Components/AddInvoice/AddInvoice.js'));
+const AddIssue = React.lazy(() => import('./Components/AddIssue/AddIssue.js'));
+const AddProject = React.lazy(() => import('./Components/AddProject/AddProject.js'));
+const AddProduct = React.lazy(() => import('./Components/AddProduct/AddProduct.js'));
 
 function App() {
   return (
@@ -16,6 +25,7 @@ function App() {
       <div style={{display:'flex', flexWrap:'wrap'}}>
           <Sidebar />
           <div style={{width:'85%'}}>
+          <Suspense fallback={<div>Loading...</div>}>
             <Routes>
               <Route path="/" exact element={<Main />} />
               <Route path="/add-user" exact element={<AddUser />} />
@@ -24,6 +34,7 @@ function App() {
               <Route path="/add-project" exact element={<AddProject />} />
               <Route path="/add-product" exact element={<AddProduct />} />
             </Routes>
+            </Suspense>
           </div>
       </div>
     </Router>
